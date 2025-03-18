35999fd879b5925532a6aa75f7820faf2961e056
23f1001493 <23f1001493@ds.study.iitm.ac.in>
2025-03-18 19:17:22 +0530

    Fix: Updated project with latest changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster.tsx";
 import Home from "@/pages/home";
 import Games from "@/pages/games";
 import NotFound from "@/pages/not-found";
